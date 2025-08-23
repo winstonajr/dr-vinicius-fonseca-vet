1efe340929445b5d22ba1ec84ad5b93b144f3c4d
winstonajr <winston.almeidamjr@gmail.com>
2025-08-23 18:29:07 -0300

    fixed: site.ts  url by site

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,10 @@
 export const siteConfig = {
   name: "Dr. Vinícius Fonseca",
-  url: "https://www.xn--vinciusfonseca-znb.vet.br", 
+  url: "https://www.drviníciusfonseca.vet.br/",
   ogImage: "/og-image.png",
   author: "Dr. Vinícius Andrade da Fonseca João",
-  keywords: "veterinário em domicílio, veterinário baixada santista, veterinário santos, veterinário são vicente, veterinário praia grande, veterinário cubatão, consulta veterinária em casa, vacina para pet em casa",
+  keywords:
+    "veterinário em domicílio, veterinário baixada santista, veterinário santos, veterinário são vicente, veterinário praia grande, veterinário cubatão, consulta veterinária em casa, vacina para pet em casa",
 };
 
 export type SiteConfig = typeof siteConfig;
